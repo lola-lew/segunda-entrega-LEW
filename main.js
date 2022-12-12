16b6c846b878799e8a21a4ce9ea3497f29b0065d
Loli <lolarlew@gmail.com>
2022-12-12 12:16:45 -0300

    no retorna nada

---
+++ main.js
@@ -0,0 +1,34 @@
+
+const listadoProductos = alert("Estos son los productos disponibles: Robot, Mono, Ciervo, Mariposas, Dragón, Chica3D, Harry Potter, Colibrí.");
+let productoIngresado = parseInt(prompt("Ingrese el nombre del producto que desea agregar al carrito e ingrese T para calcular el total: "));
+
+const articulosCarrito = [];
+
+class Producto {
+  constructor (nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
+
+const robot = new Producto ("Robot", 8.4);
+const mono = new Producto ("Mono", 7.6);
+const ciervo = new Producto ("Ciervo", 5.9);
+const mariposas = new Producto ("Mariposas", 3.2);
+const dragon = new Producto ("Dragón", 4.8);
+const chica3d = new Producto ("Chica3D", 2.7);
+const harryPotter = new Producto ("Harry Potter", 3.5);
+const colibri = new Producto ("Colibrí", 2.4);
+
+function pregunta () {
+  while (productoIngresado === Producto) {
+    return articulosCarrito.push(Producto);    
+  } if (productoIngresado === "T") {
+    return articulosCarrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+  }
+}
+
+pregunta();
+console.log(articulosCarrito);
+
+
